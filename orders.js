7b1a6cedb56ee10b79406a636a03ace68c9c7188
Rafael Fleury <144359002+RafaelFleury55@users.noreply.github.com>
2024-01-26 13:56:14 -0300

    Dashboard Responsive - HTML, CSS e JavaScript

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: 'Instalação de Câmera',
+    productNumber: '85743',
+    paymentStatus: 'Aguardando Pagamento',
+    status: 'Pendente'
+  },
+  {
+    productName: 'Instalação de Motor Deslizante',
+    productNumber: '97245',
+    paymentStatus: 'Devolução',
+    status: 'Incompleta'
+  },
+  {
+    productName: 'Instalação de Alarmes',
+    productNumber: '36452',
+    paymentStatus: 'Pago',
+    status: 'Finalizada'
+  },
+]
